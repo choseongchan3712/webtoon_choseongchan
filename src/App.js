1806b456e84aa93953c00648d023afc8ae00553a
choseongchan3712 <choseongchan3712@naver.com>
2024-10-28 02:51:31 +0900

    home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { Header } from "./components/Header";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <>
-    <header>
-      
-    </header>
-    </>
+    <HashRouter>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </HashRouter>
   );
 }
 
